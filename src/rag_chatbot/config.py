8e133e15222c899119a1665e1567475267c3d56e
markwiz <mmark.kuusik@gmail.com>
2026-03-09 10:54:34 +0200

    Inital setup config for project

---
+++ src/rag_chatbot/config.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+DATA_DIR = BASE_DIR / "data"
+RAW_DATA_DIR = DATA_DIR / "raw"
+CHROMA_DIR = BASE_DIR / "chroma_db"
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+EMBEDDING_MODEL = "text-embedding-3-large"
+CHAT_MODEL = "gpt-4.1-mini"
+COLLECTION_NAME = "tkk_knowledge_base"
+
+PDF_FILE = RAW_DATA_DIR / "kkk.pdf"
